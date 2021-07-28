79ec472acd6c5abd4927fcecf5d005c89a81ab55
Gregory Dickison <gdickison@Gregorys-MacBook-Air-2.local>
2021-07-28 15:15:22 -0700

    Generate a masonry layout of images

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,24 +1,9 @@
 import React from "react";
-import styled from "styled-components";
-
-const AppContianerDiv = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-
-  height: 100vh;
-  background: #1d1f27;
-`;
-
-const TitleH1 = styled.h1`
-  color: #ffff;
-`;
+import PictureCards from "./PictureCards";
 
 const App = () => {
   return (
-    <AppContianerDiv>
-      <TitleH1>Hello, Dave. You're looking well today.</TitleH1>
-    </AppContianerDiv>
+      <PictureCards />
   )
 };
 
---
+++ src/Components/PictureCards.js
@@ -0,0 +1,53 @@
+import styled from "styled-components";
+
+const IMAGES = [
+    "images/Cape_Kiwanda_42x42.jpeg",
+    "images/GoldRush_36x48.jpeg",
+    "images/McDonald_Creek_Falls_8_x_10.jpeg",
+    "images/Old_Man_Montana_11x14.jpeg",
+    "images/Montana_Symphony_2048x.jpeg",
+    "images/McDonald_Creek_Falls_8_x_10.jpeg",
+    "images/Spring_on_the_Clearwater_24x36.jpeg",
+    "images/The_Snow_Makes_Its_Escape_24x30.jpeg",
+    "images/Cape_Kiwanda_42x42.jpeg",
+    "images/GoldRush_36x48.jpeg",
+    "images/McDonald_Creek_Falls_8_x_10.jpeg",
+    "images/Montana_Symphony_2048x.jpeg",
+    "images/Old_Man_Montana_11x14.jpeg",
+    "images/Spring_on_the_Clearwater_24x36.jpeg",
+    "images/The_Snow_Makes_Its_Escape_24x30.jpeg",
+    "images/Cape_Kiwanda_42x42.jpeg",
+    "images/Spring_on_the_Clearwater_24x36.jpeg",
+    "images/The_Snow_Makes_Its_Escape_24x30.jpeg"
+];
+
+const PictureCardContainer = styled.div`
+    columns: 3 200px;
+    column-gap: 0;
+`;
+
+const PictureCard = styled.div`
+    padding: 2px;
+`;
+
+const Picture = styled.img`
+    display: block;
+    width: 100%;
+    height: 100%;
+`;
+
+const PictureCards = () => {
+    return (
+        <PictureCardContainer>
+            {IMAGES.map(image => (
+                <PictureCard>
+                    <a href="#">
+                        <Picture src={image} alt="pic" key={image}/>
+                    </a>
+                </PictureCard>
+            ))}
+        </PictureCardContainer>
+    )
+}
+
+export default PictureCards;
