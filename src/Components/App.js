b42e6799f03347de37ad44710eaee6671dcf888a
Gregory Dickison <gdickison@Gregorys-MacBook-Air-2.local>
2021-07-28 16:38:11 -0700

    Add top banner

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,9 +1,15 @@
 import React from "react";
+import TopBanner from "./TopBanner";
 import PictureCards from "./PictureCards";
 
 const App = () => {
   return (
+      <>
+      <TopBanner>
+            <h1 style={{lineHeight: "50px"}}>Commission a painting</h1>
+        </TopBanner>
       <PictureCards />
+      </>
   )
 };
 
---
+++ src/Components/TopBanner.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+const TopBanner = styled.div`
+    background-color: burlywood;
+    height: 50px;
+    text-align: center;
+    align-items: center;
+`;
+
+export default TopBanner;
